76afec0045f1b3df0e6bbbc09c62bab665b35de4
Start <4544242@qq.com>
2015-05-10 20:06:55 +0800

    8

---
+++ iOS/懂我/懂我/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  懂我
+//
+//  Created by Start on 5/10/15.
+//  Copyright (c) 2015 Ancun. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ iOS/懂我/懂我/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  懂我
+//
+//  Created by Start on 5/10/15.
+//  Copyright (c) 2015 Ancun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
